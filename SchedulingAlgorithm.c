d7689bc5766d4ef004663c04bd481aae6827bbea
Nikko Adrian Pacleb <93676412+Nikko-Adrian-Pacleb@users.noreply.github.com>
2021-12-16 11:03:10 -0800

    Update SchedulingAlgorithm.c

--- SchedulingAlgorithm.c
+++ SchedulingAlgorithm.c
@@ -5,12 +5,18 @@
 //Constant variables
 const int MIN_JOBS = 3;
 const int MAX_JOBS = 10;
+const int INTMAX = 2147483647;
 
 void SJN();
 void PP();
 int getJobNumber();
-int getAlgoInput();
+void getAlgoInput();
 void showTable();
+int getMinimum();
+int getTotal();
+int getProcessNumberPP();
+int getProcessNumberSJN();
+void printGanttChart();
 
 /**
     MAIN FUNCTION
@@ -64,14 +70,29 @@ void SJN(){
     int jobNumber = getJobNumber();
     int arrivalTime[jobNumber];
     int burstTime[jobNumber];
-    int index[getAlgoInput(jobNumber, arrivalTime, burstTime, NULL)];
+    getAlgoInput(jobNumber, arrivalTime, burstTime, NULL);
+    int burstTimeCopy[jobNumber];
+    for(int i = 0; i < jobNumber; ++i){
+        burstTimeCopy[i] = burstTime[i];
+    }
 
-
+    //Algorithm
+    int minArrivalTime = getMinimum(arrivalTime, jobNumber);
+    int totalBurstTime = getTotal(burstTime, jobNumber);
+    int index[totalBurstTime];
+    memset(index, -1, totalBurstTime*sizeof(index[0]));
+    for(int i = minArrivalTime; i < totalBurstTime + minArrivalTime; ++i){
+        int processNumber = getProcessNumberSJN(i, jobNumber, arrivalTime, burstTime);
+        index[i - minArrivalTime] = processNumber;
+        burstTime[processNumber]--;
+    }
 
 
 
     //@Show Data Table
-    showTable(jobNumber, arrivalTime, burstTime, NULL);
+    showTable(jobNumber, arrivalTime, burstTimeCopy, NULL);
+    //Print Gantt Chart
+    printGanttChart(index, totalBurstTime, minArrivalTime);
 }
 
 /**
@@ -88,14 +109,26 @@ void PP(){
     int arrivalTime[jobNumber];
     int burstTime[jobNumber];
     int priority[jobNumber];
-    int index[getAlgoInput(jobNumber, arrivalTime, burstTime, priority)];
-
-
-
-
+    getAlgoInput(jobNumber, arrivalTime, burstTime, priority);
+    int burstTimeCopy[jobNumber];
+    for(int i = 0; i < jobNumber; ++i){
+        burstTimeCopy[i] = burstTime[i];
+    }
+    //Algorithm
+    int minArrivalTime = getMinimum(arrivalTime);
+    int totalBurstTime = getTotal(burstTime);
+    int index[totalBurstTime];
+    memset(index, -1, totalBurstTime*sizeof(index[0]));
+    for(int i = minArrivalTime; i < totalBurstTime + minArrivalTime; ++i){
+        int processNumber = getProcessNumberPP(i, jobNumber, arrivalTime, burstTime, priority);
+        index[i - minArrivalTime] = processNumber;
+        burstTime[processNumber]--;
+    }
 
     //@Show Data Table
-    showTable(jobNumber, arrivalTime, burstTime, priority);
+    showTable(jobNumber, arrivalTime, burstTimeCopy, priority);
+    //Print Gantt Chart
+    printGanttChart(index, totalBurstTime, minArrivalTime);
 }
 
 /**
@@ -134,8 +167,7 @@ int getJobNumber(){
         Let the user input the data for the algorithm
         Return the sum of time of the jobs
 */
-int getAlgoInput(int jobNumber, int AT[], int BT[], int priority[]){
-    int jobTimeSum = 0;
+void getAlgoInput(int jobNumber, int AT[], int BT[], int priority[]){
     //Enter Data for Arrival Time Array
     printf("Arrival Time:\n");
     for(int i = 0; i < jobNumber; ++i){
@@ -147,11 +179,10 @@ int getAlgoInput(int jobNumber, int AT[], int BT[], int priority[]){
 
     //Enter Data for Burst Time Array
     printf("Burst Time:\n");
-    for(int i = 0; i < jobNumber; ++i){
-        printf("Enter Burst Time for J%d: ", (i + 1));
-        scanf("%d", &BT[i]);
+    for(int j = 0; j < jobNumber; ++j){
+        printf("Enter Burst Time for J%d: ", (j + 1));
+        scanf("%d", &BT[j]);
         fflush(stdin);
-        jobTimeSum += BT[i];
     }
     printf("\n");
 
@@ -166,7 +197,6 @@ int getAlgoInput(int jobNumber, int AT[], int BT[], int priority[]){
         }
         printf("\n");
     }
-    return jobTimeSum;
 }
 
 /**
@@ -193,4 +223,97 @@ void showTable(int jobNumber, int AT[], int BT[], int priority[]){
             printf("%d\t%d\t\t%d\t\t%d\n", (i + 1), AT[i], BT[i], priority[i]);
         }
     }
+}
+
+/**
+*/
+int getProcessNumberPP(int currentTime, int jobNumber, int arrivalTime[], int burstTime[], int priority[]){
+    int processNumber;
+    int minPriority = INTMAX;
+    for(int i = 0; i < jobNumber; ++i){
+        if(arrivalTime[i] <= currentTime && minPriority > priority[i] && burstTime[i] != 0){
+            minPriority = priority[i];
+            processNumber = i;
+        }
+    }
+    return processNumber;
+}
+
+/**
+*/
+int getProcessNumberSJN(int currentTime, int jobNumber, int arrivalTime[], int burstTime[]){
+    int processNumber;
+    int minBurstTime = INTMAX;
+    for(int i = 0; i < jobNumber; ++i){
+        if(arrivalTime[i] <= currentTime && burstTime[i] <= minBurstTime && burstTime[i] != 0){
+            minBurstTime = burstTime[i];
+            processNumber = i;
+        }
+    }
+    return processNumber;
+}
+
+/**
+*/
+void printGanttChart(int index[], int totalBurstTime, int minArrivalTime){
+    printf("Gantt Chart\n");
+    for(int i = 0; i < totalBurstTime; ++i){
+        printf("- ");
+    }
+    printf("\n");
+    int prevValue = -1;
+    for(int i = 0; i < totalBurstTime; ++i){
+        if(index[i] != prevValue){
+            printf("| ");
+            prevValue = index[i];
+        }
+        else{
+            printf("  ");
+        }
+    }
+    printf("|");
+    printf("\n");
+    prevValue = -1;
+    for(int i = 0; i < totalBurstTime; ++i){
+        if(index[i] != prevValue){
+            printf("J%d", (index[i] + 1));
+            prevValue = index[i];
+        }
+        else{
+            printf("  ");
+        }
+    }
+    printf("\n");
+    prevValue = -1;
+    for(int i = minArrivalTime; i < totalBurstTime + minArrivalTime; ++i){
+        if(index[i - minArrivalTime] != prevValue){
+            printf("%02d", i);
+            prevValue = index[i - minArrivalTime];
+        }
+        else{
+            printf("  ");
+        }
+    }
+    printf("%02d", totalBurstTime);
+    printf("\n\n");
+}
+
+/**
+*/
+int getMinimum(int array[], int num){
+    int min = INTMAX;
+    for(int i = 0; i < num; ++i){
+        if(min > array[i]) min = array[i];
+    }
+    return min;
+}
+
+/**
+*/
+int getTotal(int array[], int num){
+    int sum = 0;
+    for(int i = 0; i <num; ++i){
+        sum += array[i];
+    }
+    return sum;
 }
